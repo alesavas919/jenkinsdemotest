f6e934058031579bb9bcea1f9a090c046f93f9f3
Alejandro Salazar Anaya <alejandro.salazar@kalettre.com>
2024-05-11 00:57:16 -0500

    first commit

---
+++ src/main/java/xyz/jenkinsdemotest/xyz/JenkinsdemotestApplication.java
@@ -0,0 +1,13 @@
+package xyz.jenkinsdemotest.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdemotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsdemotestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/jenkinsdemotest/xyz/JenkinsdemotestApplicationTests.java
@@ -0,0 +1,22 @@
+package xyz.jenkinsdemotest.xyz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class JenkinsdemotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+        int resultado = 3+5;
+        assertEquals(8, resultado, "La suma no es correcta");
+
+	}
+
+	@Test
+	void context2() {
+        int resultado = 3+5;
+        assertEquals(3, resultado, "La suma no es correcta");
+	}
+}
